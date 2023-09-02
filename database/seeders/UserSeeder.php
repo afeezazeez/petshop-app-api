b598213dfd02288bc1f43ff17a881d7005336c7a
chillingloccini <chillingloccini1@gmail.com>
2023-09-02 09:03:54 +0100

    User Entity - Created migration,factory,model,seeder.
    (cherry picked from commit d44df9669d4b0c8f878da87a7edb9aa329587d5e)

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Seed buckhill admin information
+
+        User::factory()->create([
+            'first_name' => 'Buck',
+            'last_name'  => 'Hill',
+            'email' => 'admin@buckhill.co.uk',
+            'password' => 'admin',
+            'is_admin'  => 1
+        ]);
+
+        User::factory(10)->create();
+    }
+}
