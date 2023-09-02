75a7ced0bc1dc94d20f8cf4d0468a043eb4f6a7a
chillingloccini <chillingloccini1@gmail.com>
2023-09-03 00:02:38 +0100

    Wrote Command TO Reseed database as specified

---
+++ app/Console/Commands/TruncateAndSeedDatabase.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class TruncateAndSeedDatabase extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'database:truncate-and-seed';
+
+    /**
+     * This command description.
+     *
+     * @var string
+     */
+    protected $description = 'This command truncates and re-seed the db everyday at midnight UTC..';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle():void
+    {
+        Artisan::call('migrate:fresh --seed');
+    }
+}
