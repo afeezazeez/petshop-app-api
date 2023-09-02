4adb57a1d630b064c1c5353dc670db85e5d07147
chillingloccini <chillingloccini1@gmail.com>
2023-09-02 10:16:14 +0100

    Product Entity - Created Model,migration,factory and seeder

---
+++ app/Models/Product.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\UUID;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use HasFactory,UUID,SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'category_uuid',
+        'title',
+        'price',
+        'description',
+        'metadata'
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<int, string>
+     */
+    protected $casts = [
+        'metadata' => 'json'
+    ];
+}
---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\File;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $category = Category::factory()->create();
+        $brand  = Brand::factory()->create();
+        $file  = File::factory()->create();
+        return [
+            'category_uuid' => $category->uuid,
+            'title' => fake()->text(10),
+            'price' => fake()->randomFloat(2,0,3),
+            'description' => fake()->text(50),
+            'metadata'  => [
+                'brand' => $brand->uuid,
+                'image' => $file-> uuid
+            ]
+        ];
+    }
+}
