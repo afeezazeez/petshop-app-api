004711018c9bd9c8744ff1ce5ed002cec85d2e31
chillingloccini <chillingloccini1@gmail.com>
2023-09-02 13:17:54 +0100

    JWT Model updates

--- app/Models/JwtToken.php
+++ app/Models/JwtToken.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class JwtToken extends Model
 {
@@ -24,4 +26,24 @@ class JwtToken extends Model
         'last_used_at',
         'refreshed_at',
     ];
+
+    protected $casts = [
+        'expired_at' => 'datetime',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->expired_at = Carbon::now()->addMinutes(10);
+            $model->unique_id = $model->generateHashToken(6);
+        });
+    }
+
+
+    public function generateHashToken(int $length): array|string|null
+    {
+        return  hash('sha256', Str::random($length));
+    }
 }
