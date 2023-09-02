a1d5593a8fb858ca1b2fd755e0503b439c111ca4
chillingloccini <chillingloccini1@gmail.com>
2023-09-02 12:43:19 +0100

    Made quick fix to OrderFactory

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -21,34 +21,24 @@ class OrderFactory extends Factory
      */
     public function definition(): array
     {
-        $user = User::factory()->create();
-        $order_status =OrderStatus::factory()->create();
-        $payment = Payment::factory()->create();
-        $product1 =Product::factory()->create();
-        $product2 =Product::factory()->create();
+        User::factory()->create();
+        OrderStatus::factory()->create();
+        Payment::factory()->create();
+        Product::factory(2)->create();
         $shipped_at = [null,now()];
         $total_amount = fake()->randomFloat(2,100,1000);
-
-
-        $userModel = User::where('uuid', $user->uuid)->first();
-        $user_id = $userModel ? $userModel->id : null;
-
-        $orderStatusModel = User::where('uuid', $order_status->uuid)->first();
-        $order_status_id = $orderStatusModel ? $orderStatusModel->id : null;
-
-        $paymentModel = Payment::where('uuid', $payment->uuid)->first();
-        $payment_id = $paymentModel ? $paymentModel->id : null;
-
+        $product1 = Product::InRandomOrder()->first();
+        $product2 = Product::InRandomOrder()->where('id','!=',$product1->id ?? null)->first() ;
 
 
         $delivery_fee = $total_amount > 500 ? null : 15;
         return [
-            'user_id'  =>$user_id,
-            'order_status_id' => $order_status_id,
-            'payment_id'   => $payment_id,
+            'user_id'  =>User::InRandomOrder()->first()->id ?? null,
+            'order_status_id' => OrderStatus::InRandomOrder()->first()->id ?? null,
+            'payment_id'   => Payment::InRandomOrder()->first()->id ?? null,
             'products' => [
-                ['product' =>$product1->uuid , 'quantity' => 3],
-                ['product' =>$product2->uuid, 'quantity' => 2]
+                ['product' =>$product1->uuid ?? null , 'quantity' => 3],
+                ['product' =>$product2->uuid ?? null, 'quantity' => 2]
             ],
             'address'=>[
                 'billing' => fake()->address(),
