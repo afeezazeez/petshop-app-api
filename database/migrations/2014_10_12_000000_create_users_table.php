41cefddb5fb1253f1435967fc12dc4d533b3b72a
chillingloccini <chillingloccini1@gmail.com>
2023-09-02 09:04:46 +0100

    Revert "User Entity - Created migration,factory,model,seeder."
    This reverts commit d44df9669d4b0c8f878da87a7edb9aa329587d5e.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,20 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->uuid();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->boolean('is_admin')->default(0);
+            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('avatar');
-            $table->string('address');
-            $table->string('phone_number');
-            $table->boolean('is_marketing')->default(0);
             $table->rememberToken();
             $table->timestamps();
-            $table->timestamp('last_login_at');
         });
     }
 
